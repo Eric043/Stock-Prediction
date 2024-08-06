2e55eda6359ff187ed49ade3492717d41a66324a
Eric043 <95997091+Eric043@users.noreply.github.com>
2024-08-07 00:37:31 +0530

    Update views.py

--- predictor/views.py
+++ predictor/views.py
@@ -15,6 +15,7 @@ import base64
 
 # Set TensorFlow log level to suppress INFO and WARNING messages
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_TRT_DISABLE'] = '1'  # Disable TensorRT integration
 tf.get_logger().setLevel('ERROR')
 
 def get_stock_data(ticker, start_date, end_date):
